89143652b557fa1afab7cae2f1ead52ea18f47ff
Jared Grippe <jared@deadlyicon.com>
2015-07-15 23:15:38 -0700

    WIP

--- app/models/torrentz.rb
+++ app/models/torrentz.rb
@@ -23,27 +23,100 @@ class Torrentz
     providers(id).each do |name, url|
       scraper = SCRAPERS[name] or next
       magnet_link = scraper.scrape(url)
+      if magnet_link
+        puts "MAGNET FOUND FROM #{name}"
+      end
       return magnet_link if magnet_link
     end
   end
 
 
   class Scraper
-    def initialize(&parser)
-      @parser = parser
+    def self.scrape(url)
+      new(url).scrape
     end
-    def scrape(url)
-      response = HTTParty.get(url)
-      return nil if response.code != 200
-      html = Nokogiri::HTML(response.parsed_response)
-      @parser.call(html)
+    def self.domain(domain=nil)
+      @domain = domain unless domain.nil?
+      @domain
+    end
+    attr_reader :url, :respomse, :html, :document
+    def initialize(url)
+      @url = url
+    end
+    def find(css)
+      document.css(css).first
+    end
+    def url_for(path)
+      "://#{self.class.domain}#{path}"
+    end
+    def scrape
+      @response = HTTParty.get(url)
+      return nil if @response.code != 200
+      @html = @response.parsed_response
+      @document = Nokogiri::HTML(@html)
+      parse
     end
   end
 
-  SCRAPERS = {
-    'kat.cr' => Scraper.new{|html|
-      html.css('[title="Magnet link"]').first.attr(:href)
-    }
-  }
+  SCRAPERS = {}
+
+  def self.define_scraper(domain, &block)
+    scraper = Class.new(Scraper)
+    scraper.send :define_method, :parse, &block
+    scraper.domain(domain)
+    SCRAPERS[domain] = scraper
+  end
+
+  # define_scraper 'kat.cr' do
+  #   find('[title="Magnet link"]').attr(:href)
+  # end
+
+  # define_scraper 'thepiratebay.se' do
+  #   binding.pry
+  # end
+
+  define_scraper 'torrenthound.com' do
+    find('[title="Magnet download"]').attr('href')
+  end
+
+  # define_scraper 'yourbittorrent.com' do
+  #   NO MAGNET LINK
+  # end
+
+  define_scraper 'monova.org' do
+    find('#download-magnet').attr(:href)
+  end
+
+  define_scraper 'torrentreactor.com' do
+    find('#download-magnet').attr(:href)
+  end
+
+  # define_scraper 'seedpeer.me' do
+  #   binding.pry
+  # end
+
+  # define_scraper 'torrentdownloads.me' do
+  #   binding.pry
+  # end
+
+  define_scraper 'torrents.net' do
+    find('.btn2-download')[:href]
+  end
+
+  # define_scraper 'torrentfunk.com' do
+  #   binding.pry
+  # end
+
+  # define_scraper 'limetorrents.cc' do
+  #   binding.pry
+  # end
+
+  # define_scraper 'isohunt.to' do
+  #   binding.pry
+  # end
+
+  # define_scraper 'torrentproject.se' do
+  #   binding.pry
+  # end
 
 end
