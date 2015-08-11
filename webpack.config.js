8a65adf341f0a20be4eb64bd376cc98da7c590f4
Jared Grippe <jared@deadlyicon.com>
2015-08-11 14:44:03 -0700

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,32 @@
+var Webpack = require('webpack');
 var path = require('path');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var assetsPath = path.resolve(__dirname, 'public', 'assets');
+var clientPath = path.resolve(__dirname, 'client.js');
 
 module.exports = {
-  entry: "./client.js",
+  // Makes sure errors in console map to the correct file
+  // and line number
+  devtool: 'eval',
+  entry: [
+    clientPath
+  ],
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: "client.js"
+    path: assetsPath,
+    filename: 'client.js',
+    publicPath: '/assets/'
   },
   module: {
+
     loaders: [
-      { test: /\.css$/, loader: "style!css" }
+
+      // Let us also add the style-loader and css-loader, which you can
+      // expand with less-loader etc.
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+
     ]
   }
 };
